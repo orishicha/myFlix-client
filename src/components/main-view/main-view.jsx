45248c67f3fd42ecea74b4e7fd4b466534533946
orishicha <orishicha@gmail.com>
2022-01-18 01:41:20 +0100

    add registration and login
    add registration,
    add login,
    add .scss files,

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+//import PropTypes from 'prop-types';
+import './main-view.scss';
 
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -8,9 +12,11 @@ export class MainView extends React.Component {
 
     constructor() {
         super();
+// Initial state is set to null
         this.state = {
             movies: [],
-            selectedMovie: null
+            selectedMovie: null,
+            user: null
         };
     }
 
@@ -26,20 +32,44 @@ export class MainView extends React.Component {
             });
     }
 
-    setSelectedMovie(newSelectedMovie) {
+    /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
+
+    setSelectedMovie(movie) {
         this.setState({
-            selectedMovie: newSelectedMovie
+            selectedMovie: movie
+        });
+    }
+
+    /* User registers */
+    onRegistration(registration) {
+        this.setState({
+            registration,
+        });
+    }
+
+    /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+
+    onLoggedIn(user) {
+        this.setState({
+            user
         });
     }
 
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user, registration } = this.state;
 
+        if (!registration) return (<RegistrationView onRegistration={(registration) => this.onRegistration(registration)} />);
 
+        /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+        // Before the movies have been loaded
         if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
+                {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
                 {selectedMovie
                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
                     : movies.map(movie => (
